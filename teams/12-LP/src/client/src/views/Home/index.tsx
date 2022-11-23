d39c88ccc964da2c6f8056cddb54bc00fb90d028
guozhiyi01 <guozhiyi01@baidu.com>
2022-11-23 18:09:42 +0800

    feat: login with MeataMask

--- teams/12-LP/src/client/src/views/Home/index.tsx
+++ teams/12-LP/src/client/src/views/Home/index.tsx
@@ -1,9 +1,63 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {message, Button, Spin} from 'antd';
 
 const Home: React.FC = () => {
+    const [currentAccount, setCurrentAccount] = useState<string | undefined>('');
+    const [initLoad, setInitLoad] = useState<boolean>(true);
+    const [loginLoad, setLoginLoad] = useState<boolean>(false);
+    const checkAccount = async () => {
+        const {ethereum} = window;
+        if (!ethereum) {
+            message.error({
+                content: 'Make sure you have MetaMask'
+            });
+            return;
+        }
+        ethereum.request<string []>({method: 'eth_accounts'}).then((accounts) => {
+            if (accounts && accounts.length !== 0){
+                const account = accounts[0];
+                setCurrentAccount(account);
+            }
+        }).finally(() => {
+            setInitLoad(false);
+        });
+    };
+
+    const handleLogin = () => {
+        const {ethereum} = window;
+        if (!ethereum) {
+            message.error({
+                content: 'Make sure you have MetaMask'
+            });
+            return;
+        }
+        setLoginLoad(true);
+        ethereum.request<string []>({method: 'eth_requestAccounts'}).then((accounts) => {
+            if (accounts && accounts.length !== 0){
+                const account = accounts[0];
+                setCurrentAccount(account);
+            }
+        }).finally(() => {
+            setLoginLoad(false);
+        });
+    };
+
+    useEffect(() => {
+        checkAccount();
+    }, []);
+
+    const renderContent = () => {
+        return currentAccount
+            ? <div>Hello {currentAccount}</div>
+            : <Button loading={loginLoad} onClick={handleLogin}>connect to the wallet</Button>;
+    };
+
     return (
         <div>
-            hello home
+            {initLoad
+                ? <Spin />
+                : renderContent()
+            }
         </div>
     );
 };
