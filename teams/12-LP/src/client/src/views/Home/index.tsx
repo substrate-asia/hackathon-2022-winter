7477a5004c657b3af5cd8cc73051c3bd40818016
guozhiyi01 <guozhiyi01@baidu.com>
2022-11-24 16:46:14 +0800

    feat: handle login logout event

--- teams/12-LP/src/client/src/views/Home/index.tsx
+++ teams/12-LP/src/client/src/views/Home/index.tsx
@@ -1,19 +1,25 @@
 import React, {useEffect, useState} from 'react';
 import {message, Button, Spin} from 'antd';
+import {apiWrapper} from '@/utils';
 
 const Home: React.FC = () => {
     const [currentAccount, setCurrentAccount] = useState<string | undefined>('');
     const [initLoad, setInitLoad] = useState<boolean>(true);
     const [loginLoad, setLoginLoad] = useState<boolean>(false);
+
+    const {ethereum} = window;
+
     const checkAccount = async () => {
-        const {ethereum} = window;
         if (!ethereum) {
             message.error({
                 content: 'Make sure you have MetaMask'
             });
             return;
         }
-        ethereum.request<string []>({method: 'eth_accounts'}).then((accounts) => {
+        apiWrapper<string []>(
+            ethereum.request,
+            {method: 'eth_accounts'}
+        ).then((accounts) => {
             if (accounts && accounts.length !== 0){
                 const account = accounts[0];
                 setCurrentAccount(account);
@@ -32,24 +38,59 @@ const Home: React.FC = () => {
             return;
         }
         setLoginLoad(true);
-        ethereum.request<string []>({method: 'eth_requestAccounts'}).then((accounts) => {
-            if (accounts && accounts.length !== 0){
-                const account = accounts[0];
-                setCurrentAccount(account);
-            }
-        }).finally(() => {
+        apiWrapper<string []>(
+            ethereum.request,
+            {method: 'eth_requestAccounts'}
+        ).finally(() => {
             setLoginLoad(false);
         });
     };
 
+    const registListener = () => {
+        ethereum?.on('accountsChanged', (accounts) => {
+            if (!(accounts instanceof Array)) {
+                return;
+            }
+            if (accounts.length === 0) {
+                setCurrentAccount('');
+                message.success('byebye');
+                return;
+            }
+            setCurrentAccount(accounts[0]);
+            message.success('welcome!');
+        });
+    };
+
+    const cancelListener = () => {
+        ethereum?.removeAllListeners('accountsChanged');
+    };
+
     useEffect(() => {
         checkAccount();
-    }, []);
+        registListener();
+        return () => {
+            cancelListener();
+        };
+    }, [ethereum]);
+
+    const renderLoggedInContent = () => {
+        return (
+            <div>
+                <span>
+                    hello {currentAccount}
+                </span>
+            </div>
+        );
+    };
+
+    const renderLoggedOutContent = () => {
+        return <Button loading={loginLoad} onClick={handleLogin}>connect to the wallet</Button>;
+    };
 
     const renderContent = () => {
         return currentAccount
-            ? <div>Hello {currentAccount}</div>
-            : <Button loading={loginLoad} onClick={handleLogin}>connect to the wallet</Button>;
+            ? renderLoggedInContent()
+            : renderLoggedOutContent();
     };
 
     return (
