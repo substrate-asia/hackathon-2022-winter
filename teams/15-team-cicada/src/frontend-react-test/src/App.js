9e27b1a870af3c71b7379275d0c5988f35d6e373
阿斗共享 <30933867+makefriendwithtime@users.noreply.github.com>
2022-12-13 17:44:25 +0800

    12.13-crg

--- teams/15-team-cicada/src/frontend-react-test/src/App.js
+++ teams/15-team-cicada/src/frontend-react-test/src/App.js
@@ -1,32 +1,102 @@
-import Index from "./views/index";
-import Create from "./Create";
-import { Link, Route, Switch } from "react-router-dom";
+import React, { createRef } from "react";
+import {
+  Container,
+  Dimmer,
+  Loader,
+  Grid,
+  Sticky,
+  Message,
+} from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
 
-function App() {
+import { SubstrateContextProvider, useSubstrateState } from "./substrate-lib";
+import { DeveloperConsole } from "./substrate-lib/components";
+
+import AccountSelector from "./AccountSelector";
+import Floor from "./Floor";
+import Balances from "./Balances";
+import BlockNumber from "./BlockNumber";
+import Events from "./Events";
+import Interactor from "./Interactor";
+import Metadata from "./Metadata";
+import NodeInfo from "./NodeInfo";
+// import TemplateModule from './TemplateModule'
+import CicadaModule from "./CicadaModule";
+import Transfer from "./Transfer";
+import Upgrade from "./Upgrade";
+
+function Main() {
+  const { apiState, apiError, keyringState } = useSubstrateState();
+
+  const loader = (text) => (
+    <Dimmer active>
+      <Loader size="small">{text}</Loader>
+    </Dimmer>
+  );
+
+  const message = (errObj) => (
+    <Grid centered columns={2} padded>
+      <Grid.Column>
+        <Message
+          negative
+          compact
+          floating
+          header="Error Connecting to Substrate"
+          content={`Connection to websocket '${errObj.target.url}' failed.`}
+        />
+      </Grid.Column>
+    </Grid>
+  );
+
+  if (apiState === "ERROR") return message(apiError);
+  else if (apiState !== "READY") return loader("Connecting to Substrate");
+
+  if (keyringState !== "READY") {
+    return loader(
+      "Loading accounts (please review any extension's authorization)"
+    );
+  }
+
+  const contextRef = createRef();
   return (
-    <div>
-      {/* <div>
-        <p> */}
-      <Route exact component={Index} path="/"></Route>
-      {/* </p>
-        <p> */}
-      <Route exact component={Create} path="/Create"></Route>
-      {/* </p>
-      </div> */}
-      {/* <hr /> */}
-      {/* <div>
-        <Switch>
-          <Route exact component={Index} path="/"></Route>
-
-          <Route
-            exact
-            component={Create}
-            path="/substrate-front-end-template/Create"
-          ></Route>
-        </Switch>
-      </div> */}
+    <div ref={contextRef}>
+      {/* <Sticky context={contextRef}> */}
+      <AccountSelector />
+      {/* </Sticky> */}
+      <Container>
+        <Grid stackable columns="equal">
+          <Grid.Row stretched>
+            <NodeInfo />
+            <Metadata />
+            <BlockNumber />
+            <BlockNumber finalized />
+          </Grid.Row>
+          <Grid.Row stretched>
+            <Balances />
+          </Grid.Row>
+          <Grid.Row>
+            <Transfer />
+            <Interactor />
+            {/* <Upgrade /> */}
+          </Grid.Row>
+          <Grid.Row>
+            <CicadaModule />
+            <Events />
+          </Grid.Row>
+        </Grid>
+      </Container>
+      <DeveloperConsole />
+      {/* <Sticky context={contextRef}>
+      </Sticky> */}
+      <Floor />
     </div>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <SubstrateContextProvider>
+      <Main />
+    </SubstrateContextProvider>
+  );
+}
