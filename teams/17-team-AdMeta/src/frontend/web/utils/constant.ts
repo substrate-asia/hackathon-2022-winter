3e590043f859b296a211aff938238f287e57b1c0
wxd56987 <672514143@qq.com>
2022-12-26 18:46:20 +0800

    admeta web extension

---
+++ teams/17-team-AdMeta/src/frontend/web/utils/constant.ts
@@ -0,0 +1,22 @@
+export const SEO_DEFAULT_TITLE = 'AdMeta | A privacy-preserving Ad Platform in Metaverse'
+export const SEO_DEFAULT_KEYWORDS = 'AdMeta, Metaverse advertisement, advertising platform, web3, web3 advertising'
+export const SEO_DEFAULT_DESCRIPTION = `AdMeta is a Metaverse ad platform that focuses on privacy preserving. We use a Trusted Execution Environment (TEE) to store and process user's private data for privacy protection.`
+
+export const EXTENSION_NAME = 'AdMetaHackathon'
+export const POLKADOT_NETWORK = 'wss://testnet.admeta.network'
+
+export const ADMETA_MSG_HACKATHON_ACCOUNT = 'ADMETA_MSG_HACKATHON_ACCOUNT'
+export const ADMETA_MSG_HACKATHON_SOUL = 'ADMETA_MSG_HACKATHON_SOUL'
+
+export const NEXT_PUBLIC_RECAPTCHA_SITE_KEY = '6Lf7VCUjAAAAAAAUag26CfXu0_oretpWmri5LxUC'
+export const RECAPTCHA_SECRET_KEY = '6Lf7VCUjAAAAAPg5DrN6TjT5auPB69p90pGBekSa'
+
+export const API = 'http://localhost:3120/hackathon/'
+
+export const IPFS_IMGS = [
+  'https://ipfs.io/ipfs/QmPQdVU1riwzijhCs1Lk6CHmDo4LpmwPPLuDauY3i8gSzL',
+  'https://ipfs.io/ipfs/QmTXuAUW4nLqRZ8NCbEaH8tPSYVhqq3hPTYbnqx8p97Yi7',
+  'https://ipfs.io/ipfs/QmNdvtT9EmrUc6haJyN7ZanHNrsjd23v1ydG6r8jTGEZvq',
+  'https://ipfs.io/ipfs/QmZnyd5V5UdCdyPEZX5fCJFi1Dhr4nQjwQXBE3KXBzQnv3',
+  'https://ipfs.io/ipfs/QmXcLJTFGZAZpta84zXqGFao1HMbFdoMntKT5MRrLnZt91'
+]
