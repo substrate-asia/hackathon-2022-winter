b44009913733d7faaee3688bf5fc451ad6c17db3
xiyu1984 <shanellyy@126.com>
2023-01-07 09:57:18 +0800

    update docs

--- teams/38-O20k/src/O-AMM/constgrad.py
+++ teams/38-O20k/src/O-AMM/constgrad.py
@@ -1,7 +1,112 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 import solving
+
+def drawPoints(axes, x_points, b, C):
+    # func_symmetry = np.vectorize(solving.solve_symmetry_const_gradient)
+    # y_symmetry = func_symmetry(x_points, b, C)
+    
+    for i in range(len(x_points)):
+        x_show2 = x_points[i]
+        print(x_show2)
+        y_show2 = solving.solve_symmetry_const_gradient(x_show2, b, C)
+        print(y_show2)
+
+        axes.scatter(x_show2, y_show2, s=10, c='red')
+        axes.vlines(float(x_show2), 0., float(y_show2), color='lightblue', linestyles = ':', linewidth =0.5)
+        axes.hlines(float(y_show2), 0., float(x_show2), color='lightblue', linestyles = ':', linewidth =0.5)
+        axes.text(x_show2, y_show2, 'Δx= {}, Δy={}'.format(x_show2, '{:,.{}f}'.format(y_show2, 2)), c='darkgrey')
+
+
+def animationPoints(fig, axes, x_show, y_show):
+    # assert(len(x_show) == len(y_show))
+
+    frames = []
+    for i in range(len(x_show)):
+        x_show2 = x_show[i]
+        y_show2 = y_show[i]
+        # frame = axes.vlines(float(x_show2), 0., float(y_show2), color='lightblue', linestyles = ':', linewidth =0.5, animated=True)
+        # frame = axes.hlines(float(y_show2), 0., float(x_show2), color='lightblue', linestyles = ':', linewidth =0.5, animated=True)
+        # axes.text(x_show2, y_show2, 'Δx= {}, Δy={}'.format(x_show2, '{:,.{}f}'.format(y_show2, 2)), c='darkgrey', animated=True)
+        frame = axes.plot(x_show2, y_show2, 'o', c='red', animated=True)
+        frames.append(frame)
+        # axes.draw_artist(frame)
+    
+    ani = animation.ArtistAnimation(fig, frames, interval=100, blit=True,
+                                repeat=True)
+
+
+def testAnimation():
+    x0 = np.linspace(0, 10, 100)
+    y0 = -1 * x0 + 10
+
+    fig, axes = plt.subplots(1, 1)
+
+    axes.plot(x0, y0)
+
+    # animationPoints(fig, axes, x0, y0)
+
+    frames = []
+    for i in range(len(x0)):
+        x_show2 = x0[i]
+        y_show2 = y0[i]
+        # frame = axes.vlines(float(x_show2), 0., float(y_show2), color='lightblue', linestyles = ':', linewidth =0.5, animated=True)
+        # frame = axes.hlines(float(y_show2), 0., float(x_show2), color='lightblue', linestyles = ':', linewidth =0.5, animated=True)
+        # axes.text(x_show2, y_show2, 'Δx= {}, Δy={}'.format(x_show2, '{:,.{}f}'.format(y_show2, 2)), c='darkgrey', animated=True)
+        frame = axes.plot(x_show2, y_show2, 'o', c='red', animated=True)
+        frames.append(frame)
+        # axes.draw_artist(frame)
+    
+    ani = animation.ArtistAnimation(fig, frames, interval=100, blit=True,
+                                repeat=True)
+
+    plt.show()
+
+
+def testAnimation2():
+    x0 = np.linspace(0, 10, 100)
+    y0 = -1 * x0 + 10
+
+    fig, axes = plt.subplots(1, 1)
+
+    axes.plot(x0, y0)
+
+    line = axes.plot(x0[0], y0[0], 'o', c='red')[0]
+
+    vline = axes.plot([0, 0], [0, 0], ':', color='lightblue', linewidth =0.8)[0]
+    hline = axes.plot([0, 0], [0, 0], ':', color='lightblue', linewidth =0.8)[0]
+
+    text = axes.text(x0[0], y0[0], 'Δx= {}, Δy={}'.format('{:,.{}f}'.format(x0[0], 2), '{:,.{}f}'.format(y0[0], 2)), c='darkgrey')
+
+    def animate(i):
+        x_show2 = x0[i]
+        y_show2 = y0[i]
+
+        line.set_data(x_show2, y_show2)
+        text.set_position((x_show2, y_show2))
+        text.set_text('Δx= {}, Δy={}'.format('{:,.{}f}'.format(x0[i], 2), '{:,.{}f}'.format(y0[i], 2)))
+        # vline = axes.vlines(float(x_show2), 0., float(y_show2), color='lightblue', linestyles = ':', linewidth =0.5)
+        # hline = axes.hlines(float(y_show2), 0., float(x_show2), color='lightblue', linestyles = ':', linewidth =0.5)
+        vline.set_data([x_show2, x_show2], [0, y_show2]);
+        hline.set_data([0, x_show2], [y_show2, y_show2]);
+
+        return line, text, vline, hline
+    
+    ani = animation.FuncAnimation(fig, animate, len(y0), interval=100, blit=True,
+                                repeat=True)
+    
+    writergif = animation.PillowWriter(fps=10)
+    ani.save('./curve.gif',writer=writergif)
+
+    # plt.rcParams['animation.ffmpeg_path'] ='./tool/bin/ffmpeg.exe'
+    # FFwriter=animation.FFMpegWriter(fps=10, extra_args=['-vcodec', 'libx264'])
+    # ani.save("movie.mp4", writer=FFwriter)
+
+    plt.show()
+
 
 def draw():
     initX = 10
@@ -52,6 +157,9 @@ def draw():
     axes.plot(x_symmetry, y_symmetry, label="O-AMM", linewidth=1.5, color='lightcyan')
     axes.plot(x_curve, y_curve, label="Curve", linewidth=0.75)
 
+    # points = np.array([3., 5., 10., 14.1421, 18.39, 26.94, 36.9])
+    # drawPoints(axes, points, b, initC1)
+
     axes.set_xlabel('Reserve X', color="white")
     axes.set_ylabel('Reserve Y', color="white")
     axes.spines['left'].set_color("white")
@@ -60,6 +168,44 @@ def draw():
     axes.set_aspect(1)
     axes.legend(loc='upper right')
     plt.style.use("dark_background")
+
+    # Animation
+    dy_dx_expr = solving.diff_o_amm_dy_dx(b, initC1)
+
+    line = axes.plot(x_symmetry[0], y_symmetry[0], 'o', c='red')[0]
+
+    dy_dx = solving.price_o_amm(x_symmetry[0], y_symmetry[0], dy_dx_expr)
+    text = axes.text(x_symmetry[0], y_symmetry[0], 'x= {}, y={}\n Price=dy/dx={}'.format('{:,.{}f}'.format(x_symmetry[0], 2), '{:,.{}f}'.format(y_symmetry[0], 2), '{:,.{}f}'.format(dy_dx, 2)), c='darkgrey')
+
+    vline = axes.plot([0, 0], [0, 0], ':', color='lightblue', linewidth =0.8)[0]
+    hline = axes.plot([0, 0], [0, 0], ':', color='lightblue', linewidth =0.8)[0]
+
+    def animate(i):
+        x_show2 = x_symmetry[i]
+        y_show2 = y_symmetry[i]
+
+        line.set_data(x_show2, y_show2)
+        text.set_position((x_show2, y_show2))
+
+        dy_dx = solving.price_o_amm(x_show2, y_show2, dy_dx_expr)
+        text.set_text('x= {}, y={}\n Price=dy/dx={}'.format('{:,.{}f}'.format(x_show2, 2), '{:,.{}f}'.format(y_show2, 2), '{:,.{}f}'.format(dy_dx, 2)))
+        vline.set_data([x_show2, x_show2], [0, y_show2]);
+        hline.set_data([0, x_show2], [y_show2, y_show2]);
+
+        return line, text, vline, hline
+    
+    ani = animation.FuncAnimation(fig, animate, len(y0), interval=200, blit=True,
+                                repeat=True)
+    # Animation end
+
+    # writergif = animation.PillowWriter(fps=10)
+    # ani.save('./o-amm-curve.gif',writer=writergif)
+
+    # plt.rcParams['animation.ffmpeg_path'] ='./tool/bin/ffmpeg.exe'
+    # FFwriter=animation.FFMpegWriter(fps=10, extra_args=['-vcodec', 'libx264'])
+    # ani.save("./o-amm-movie.mp4", writer=FFwriter)
+
     plt.show()
 
 draw()
+# testAnimation2()
--- teams/38-O20k/src/O-AMM/solving.py
+++ teams/38-O20k/src/O-AMM/solving.py
@@ -1,4 +1,4 @@
-from sympy import symbols, Eq, solve
+from sympy import symbols, Eq, solve, idiff
 
 # `alpha` is the quotient of the geometric mean over arithmetic mean,
 # which is a way to quantify the degree of difference between x and y
@@ -30,3 +30,18 @@ def solve_symmetry_const_gradient(x, b, C):
     s = solve(eq)
     return s[0]
 
+def diff_o_amm_dy_dx(b, C):
+    y = symbols('y', real=True)
+    x = symbols('x', real=True)
+
+    _alpha = alpha(x, y)
+
+    z = _alpha*(-(x**2+y**2)+b*(x+y)) + 2*(1-_alpha)*C - 2*x*y
+    # print(idiff(z, y, x))
+    return idiff(z, y, x)
+
+
+def price_o_amm(x_val, y_val, dy_dx_expr):
+    y = symbols('y', real=True)
+    x = symbols('x', real=True)
+    return dy_dx_expr.subs({y: y_val, x: x_val});
